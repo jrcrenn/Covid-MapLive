2c791bc7abe8d1fe9aefee7798265c7fd835dd7b
Marian Bret <marian.bret@epitech.eu>
2020-04-14 14:13:47 +0200

    clean app controller

--- covid_back/src/app.controller.ts
+++ covid_back/src/app.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, HttpStatus, HttpException, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Response } from 'express';
-import * as AboutJson from './models/json/about.json';
 
 @Controller()
 export class AppController {
@@ -10,14 +9,5 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-
-  @Get('about.json')
-  getAboutJson(@Res() res: Response) {
-    try {
-        res.status(HttpStatus.OK).json(AboutJson);
-    } catch (err) {
-        throw new HttpException(err, HttpStatus.BAD_REQUEST);
-    }
   }
 }
