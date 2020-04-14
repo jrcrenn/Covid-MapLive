efda68aca6fd0c330eb9f73b924c7ae7492e9ad5
Marian Bret <marian.bret@epitech.eu>
2020-04-14 13:59:09 +0200

    add authentification module -> gestion entrée utilisateurs en base / signin / signup /update

---
+++ covid_back/src/auth/auth.module.ts
@@ -0,0 +1,26 @@
+import { Module, HttpModule } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../models/user/users.entity';
+import { UsersService } from '../users/users.service';
+import { JwtStrategy } from './strategy/jwt.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { APP_CONFIG } from '../config/app.config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: APP_CONFIG.secretKey,
+      signOptions: { algorithm: 'HS512', expiresIn: APP_CONFIG.expiresIn },
+    }),
+    HttpModule
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, UsersService, JwtStrategy],
+  exports: [PassportModule, JwtModule],
+})
+export class AuthModule {}
