06af0183f961d2c484be05f40088f891b19ef968
Marian Bret <marian.bret@epitech.eu>
2020-04-15 21:19:24 +0200

    [update] user module for entity settings typeorm

--- covid_back/src/users/users.module.ts
+++ covid_back/src/users/users.module.ts
@@ -4,9 +4,10 @@ import { User } from '../models/user/users.entity';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { AuthModule } from '../auth/auth.module';
+import { Settings } from '../models/user/settings.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), AuthModule],
+  imports: [TypeOrmModule.forFeature([User, Settings]), AuthModule],
   providers: [UsersService],
   controllers: [UsersController],
   exports: [UsersService],
