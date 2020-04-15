383fd8fdea7d2033607e08cb7e673795d092ba97
Marian Bret <marian.bret@epitech.eu>
2020-04-15 21:15:57 +0200

    add settings DTO and settings entity

---
+++ covid_back/src/models/user/settings.dto.ts
@@ -0,0 +1,9 @@
+import { Allow, IsEmail, IsNotEmpty } from 'class-validator';
+import { User } from './users.entity';
+export class SettingsDto {
+    @IsNotEmpty()
+    type: string;
+
+    @IsNotEmpty()
+    options: string;
+}
---
+++ covid_back/src/models/user/settings.entity.ts
@@ -0,0 +1,15 @@
+import { Column, Entity, PrimaryGeneratedColumn, OneToMany, ManyToOne } from 'typeorm';
+import { User } from './users.entity';
+
+@Entity()
+export class Settings {
+  @PrimaryGeneratedColumn()
+  id?: number;
+
+  @Column({ nullable: true }) type?: string;
+
+  @Column({ nullable: true }) options?: string;
+
+  @ManyToOne(() => User, user => user.id)
+  user?: User;
+}
