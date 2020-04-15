7039900cb3e9586ac6c331383d0a336d77b0daec
Marian Bret <marian.bret@epitech.eu>
2020-04-15 21:16:32 +0200

    add entity in app module

--- covid_back/src/app.module.ts
+++ covid_back/src/app.module.ts
@@ -7,10 +7,11 @@ import { DATABASE_CONFIG } from './config/database.config';
 import { User } from './models/user/users.entity';
 import { AuthModule } from './auth/auth.module';
 import { ApiModule } from './api/api.module';
+import { Settings } from './models/user/settings.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({ ...DATABASE_CONFIG, entities: [User] }),
+    TypeOrmModule.forRoot({ ...DATABASE_CONFIG, entities: [User, Settings] }),
     UsersModule,
     AuthModule,
     ApiModule
